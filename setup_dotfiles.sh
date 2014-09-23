f60a89a35473c1648f1ec4d17e968ed47339b213
Sebastien Delisle <seb0del@gmail.com>
2014-09-23 12:47:50 -0400

    basic setup and added bashrc and vimrc.

---
+++ setup_dotfiles.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+
+DOT_FILE_LIST="bashrc vimrc" # .file names without the dot.
+DOT_BACK="${HOME}/.dotfiles_backup"
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+mkdir -p ${DOT_BACK}
+
+for file in $DOT_FILE_LIST;
+do
+    mv ~/.$file ${DOT_BACK}
+    ln -s $DIR/$file ~/.$file
+done
