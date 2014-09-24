903fecdbb159e1fb37a13b7b92ff4530638b829a
Sebastien Delisleeee <seb0del@gmail.commmm>
2014-09-23 20:10:35 -0400

    added .gitconfig to the list of dotfiles to be setup.

--- setup_dotfiles.sh
+++ setup_dotfiles.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 
-DOT_FILE_LIST="bashrc vimrc" # .file names without the dot.
+DOT_FILE_LIST="bashrc vimrc gitconfig" # .file names without the dot.
 DOT_BACK="${HOME}/.dotfiles_backup"
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
