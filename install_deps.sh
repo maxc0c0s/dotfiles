52a15b38dcae8491fa71cf382282e2432bd4fe3e
Sebastien Delisle <maxc0c0s@users.noreply.github.com>
2014-10-19 19:01:24 -0400

    Update install_deps.sh
    added -y

--- install_deps.sh
+++ install_deps.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 sudo apt-get update
-sudo apt-get install vim
-sudo apt-get install python-pip
+sudo apt-get install -y vim
+sudo apt-get install -y python-pip
 
 pip install virtualenvwrapper
