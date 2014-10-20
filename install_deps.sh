f15e0bbbb7081c2e3a348c8105f9e78aa85fc5cb
Sebastien <seb0del@gmail.com>
2014-10-20 10:42:15 -0400

    python requirement are now installed using a requirement file

--- install_deps.sh
+++ install_deps.sh
@@ -4,10 +4,4 @@ sudo apt-get update
 sudo apt-get install -y vim
 sudo apt-get install -y python-pip
 
-sudo pip install virtualenvwrapper
-sudo pip install cookiecutter
-sudo pip install coverage
-sudo pip install sphinx
-sudo pip install flake8
-sudo pip install tox
-sudo pip install wheel
+sudo pip install -r requirements.txt
